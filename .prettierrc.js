23d3068ec74fc5ade90536d1eeac7cd751ece0c8
Notorious <cristee95@gmail.com>
2021-02-07 17:23:16 +0200

    added eslint, cleaned up, removed deprecated methods

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  endOfLine: 'lf',
+  semi: true,
+  arrowParens: 'always',
+  printWidth: 110
+};
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const bodyParser= require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+// const bodyParser = require('body-parser');
+const {MongoClient} = require('mongodb');
 const cors = require('cors');
 
 const app = express();
 const port = 3000;
 const clientId = 'fum4';
 const clientSecret = '1b2duj35';
-const mongoUri = `mongodb+srv://${clientId}:${clientSecret}@cluster0.v48nx.mongodb.net/smil32-db?retryWrites=true&w=majority`; //?authSource=admin
-const corsOptions = { origin: false };
+const mongoUri = `mongodb+srv://${clientId}:${clientSecret}@cluster0.v48nx.mongodb.net/smil32-db?retryWrites=true&w=majority`; // ?authSource=admin
+const corsOptions = {origin: false};
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
@@ -20,29 +20,29 @@ app.use(function (req, res, next) {
 });
 
 const success = (res, payload) => {
-  const { matchedCount, modifiedCount } = res;
+  const {matchedCount, modifiedCount} = res;
 
   console.log('#### success ::: matchedCount / modifiedCount : ', matchedCount, modifiedCount);
 
   res.json(payload);
   res.status(200);
-}
+};
 
 const error = (res, error) => {
-  const { matchedCount, modifiedCount } = res;
+  const {matchedCount, modifiedCount} = res;
 
   console.log('#### error ::: matchedCount / modifiedCount : ', matchedCount, modifiedCount);
 
   console.log('#### query response -> ', res);
   console.log('#### error -> ', error);
   res.status(500);
-}
+};
 
 const logErrorConnecting = (err, client) => {
   console.log('#### error connecting to db', err, client);
   console.log('#### error -> ', err);
   console.log('#### client -> ', client);
-}
+};
 
 MongoClient.connect(mongoUri, (err, client) => {
   console.log('#### connected ');
@@ -53,74 +53,81 @@ MongoClient.connect(mongoUri, (err, client) => {
     const clientsCollection = db.collection('clients');
 
     app.use(cors(corsOptions));
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.json());
 
     app.get('/clients', (req, res) => {
-      clientsCollection.find().sort({ 'name': 1, 'surname': 1 }).toArray()
-        .then(results => success(res, results))
-        .catch(err => error(err));
+      clientsCollection
+        .find()
+        .sort({name: 1, surname: 1})
+        .toArray()
+        .then((results) => success(res, results))
+        .catch((err) => error(err));
     });
 
     app.post('/client', (req, res) => {
-      clientsCollection.insertOne(req.body)
-        .then(results => success(res, results))
-        .catch(err => error(err));
+      clientsCollection
+        .insertOne(req.body)
+        .then((results) => success(res, results))
+        .catch((err) => error(err));
     });
 
     app.post('/appointment', (req, res) => {
-      const { client, appointment, control, date, price, technician, treatment } = req.body;
-      const query = { '_id': client };
+      const {client, appointment, control, date, price, technician, treatment} = req.body;
+      const query = {_id: client};
       const update = {
-        '$push': {
-          'appointments': {
-            'appointment': appointment,
-            'control': control,
-            'date': date,
-            'price': price,
-            'technician': technician,
-            'treatment': treatment,
+        $push: {
+          appointments: {
+            appointment,
+            control,
+            date,
+            price,
+            technician,
+            treatment
           }
         }
       };
 
-      clientsCollection.findOneAndUpdate(query, update)
-        .then(results => success(res, results))
-        .catch(err => error(err));
+      clientsCollection
+        .findOneAndUpdate(query, update)
+        .then((results) => success(res, results))
+        .catch((err) => error(err));
     });
 
     app.get('/appointments', (req, res) => {
-      clientsCollection.aggregate([
-        {$unwind:'$appointments'},
-        {$sort:{'appointments.date': 1}},
-        {$project: { 'name': 1, 'surname': 1, 'appointment': '$appointments' } }
-      ]).toArray()
-        .then(results => success(res, results))
-        .catch(err => error(err));
+      clientsCollection
+        .aggregate([
+          {$unwind: '$appointments'},
+          {$sort: {'appointments.date': 1}},
+          {$project: {appointment: '$appointments', name: 1, surname: 1}}
+        ])
+        .toArray()
+        .then((results) => success(res, results))
+        .catch((err) => error(err));
     });
 
     app.delete('/appointment', (req, res) => {
-      const { date } = req.body;
-      const query = { 'appointments.date': date };
-      const update = { $pull: { 'appointments': { 'date': date } } };
+      const {date} = req.body;
+      const query = {'appointments.date': date};
+      const update = {$pull: {appointments: {date}}};
 
-      clientsCollection.updateOne(query, update)
-        .then(results => success(res, results))
-        .catch(err => error(err));
+      clientsCollection
+        .updateOne(query, update)
+        .then((results) => success(res, results))
+        .catch((err) => error(err));
     });
 
     app.delete('/client', (req, res) => {
-      clientsCollection.deleteOne(
-        { _id: req.body.clientId }
-      )
-        .then(results => success(res, results))
-        .catch(err => error(err));
+      clientsCollection
+        .deleteOne({_id: req.body.clientId})
+        .then((results) => success(res, results))
+        .catch((err) => error(err));
     });
 
     app.put('/appointment', (req, res) => {
-      const { dateIndex, appointment, control, date, price, technician, treatment } = req.body;
-      const query = { 'appointments.date': dateIndex };
-      const options = { arrayFilters: [{ 'element.date': dateIndex }] };
+      const {dateIndex, appointment, control, date, price, technician, treatment} = req.body;
+      const query = {'appointments.date': dateIndex};
+      const options = {arrayFilters: [{'element.date': dateIndex}]};
       const update = {
         $set: {
           'appointments.$[element].appointment': appointment,
@@ -128,19 +135,20 @@ MongoClient.connect(mongoUri, (err, client) => {
           'appointments.$[element].date': date,
           'appointments.$[element].price': price,
           'appointments.$[element].technician': technician,
-          'appointments.$[element].treatment': treatment,
+          'appointments.$[element].treatment': treatment
         }
       };
 
-      clientsCollection.updateOne(query, update, options)
-        .then(results => success(res, results))
-        .catch(err => error(err));
+      clientsCollection
+        .updateOne(query, update, options)
+        .then((results) => success(res, results))
+        .catch((err) => error(err));
     });
   } else {
     logErrorConnecting(err, client);
   }
-})
+});
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
