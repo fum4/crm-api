cbc491be21b7ca310c797b1e26e9b2c8d88ffd65
Alexandru Andronache <alexandru.andronache@iquestgroup.com>
2021-02-08 04:35:42 +0200

    Add authentication

--- index.js
+++ index.js
@@ -2,21 +2,6 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
 
-const app = express();
-const port = 3000;
-const clientId = 'fum4';
-const clientSecret = '1b2duj35';
-const mongoUri = `mongodb+srv://${clientId}:${clientSecret}@cluster0.v48nx.mongodb.net/smil32-db?retryWrites=true&w=majority`; // ?authSource=admin
-const corsOptions = {origin: false};
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  next();
-});
 
 const success = (res, payload) => {
   const {matchedCount, modifiedCount} = res;
@@ -42,110 +27,135 @@ const logErrorConnecting = (err, client) => {
   console.log('#### client -> ', client);
 };
 
-MongoClient.connect(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
-  console.log('#### connected ');
+const buildMongoUri = (username, password) => {
+  return `mongodb+srv://${username}:${password}@cluster0.v48nx.mongodb.net/smil32-db?retryWrites=true&w=majority`;
+}
 
-  const db = client && client.db('smil32-db');
+const app = express();
+const port = 3000;
+const corsOptions = { origin: false };
 
-  if (db) {
-    const clientsCollection = db.collection('clients');
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
-    app.use(cors(corsOptions));
-    app.use(express.urlencoded({extended: true}));
-    app.use(express.json());
-
-    app.get('/clients', (req, res) => {
-      clientsCollection
-        .find()
-        .sort({name: 1, surname: 1})
-        .toArray()
-        .then((results) => success(res, results))
-        .catch((err) => error(err));
-    });
-
-    app.post('/client', (req, res) => {
-      clientsCollection
-        .insertOne(req.body)
-        .then((results) => success(res, results))
-        .catch((err) => error(err));
-    });
-
-    app.delete('/client', (req, res) => {
-      clientsCollection
-        .deleteOne({_id: req.body.clientId})
-        .then((results) => success(res, results))
-        .catch((err) => error(err));
-    });
-
-    app.get('/appointments', (req, res) => {
-      clientsCollection
-        .aggregate([
-          {$unwind: '$appointments'},
-          {$sort: {'appointments.date': 1}},
-          {$project: {appointment: '$appointments', name: 1, surname: 1}}
-        ])
-        .toArray()
-        .then((results) => success(res, results))
-        .catch((err) => error(err));
-    });
-
-    app.post('/appointment', (req, res) => {
-      const {client, appointment, control, date, price, technician, treatment} = req.body;
-      const query = {_id: client};
-      const update = {
-        $push: {
-          appointments: {
-            appointment,
-            control,
-            date,
-            price,
-            technician,
-            treatment
-          }
-        }
-      };
-
-      clientsCollection
-        .updateOne(query, update)
-        .then((results) => success(res, results))
-        .catch((err) => error(err));
-    });
-
-    app.put('/appointment', (req, res) => {
-      const {dateIndex, appointment, control, date, price, technician, treatment} = req.body;
-      const query = {'appointments.date': dateIndex};
-      const options = {arrayFilters: [{'element.date': dateIndex}]};
-      const update = {
-        $set: {
-          'appointments.$[element].appointment': appointment,
-          'appointments.$[element].control': control,
-          'appointments.$[element].date': date,
-          'appointments.$[element].price': price,
-          'appointments.$[element].technician': technician,
-          'appointments.$[element].treatment': treatment
-        }
-      };
-
-      clientsCollection
-        .updateOne(query, update, options)
-        .then((results) => success(res, results))
-        .catch((err) => error(err));
-    });
-
-    app.delete('/appointment', (req, res) => {
-      const {date} = req.body;
-      const query = {'appointments.date': date};
-      const update = {$pull: {appointments: {date}}};
-
-      clientsCollection
-        .updateOne(query, update)
-        .then((results) => success(res, results))
-        .catch((err) => error(err));
-    });
-  } else {
-    logErrorConnecting(err, client);
-  }
+  next();
 });
+
+app.post('/auth', (request, response) => {
+  const { username, password } = request.body;
+  const mongoUri = buildMongoUri(username, password);
+
+  MongoClient.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    console.log('#### connected ');
+
+    success(response);
+
+    const db = client && client.db('smil32-db');
+
+    if (db) {
+      const clientsCollection = db.collection('clients');
+
+      app.get('/clients', (req, res) => {
+        clientsCollection
+          .find()
+          .sort({name: 1, surname: 1})
+          .toArray()
+          .then((results) => success(res, results))
+          .catch((err) => error(err));
+      });
+
+      app.post('/client', (req, res) => {
+        clientsCollection
+          .insertOne(req.body)
+          .then((results) => success(res, results))
+          .catch((err) => error(err));
+      });
+
+      app.delete('/client', (req, res) => {
+        clientsCollection
+          .deleteOne({_id: req.body.clientId})
+          .then((results) => success(res, results))
+          .catch((err) => error(err));
+      });
+
+      app.get('/appointments', (req, res) => {
+        clientsCollection
+          .aggregate([
+            {$unwind: '$appointments'},
+            {$sort: {'appointments.date': 1}},
+            {$project: {appointment: '$appointments', name: 1, surname: 1}}
+          ])
+          .toArray()
+          .then((results) => success(res, results))
+          .catch((err) => error(err));
+      });
+
+      app.post('/appointment', (req, res) => {
+        const {client, appointment, control, date, price, technician, treatment} = req.body;
+        const query = {_id: client};
+        const update = {
+          $push: {
+            appointments: {
+              appointment,
+              control,
+              date,
+              price,
+              technician,
+              treatment
+            }
+          }
+        };
+
+        clientsCollection
+          .updateOne(query, update)
+          .then((results) => success(res, results))
+          .catch((err) => error(err));
+      });
+
+      app.put('/appointment/:id', (req, res) => {
+        const { appointment, control, date, price, technician, treatment } = req.body;
+        const { id } = req.params;
+        const query = { 'appointments.date': id };
+        const options = { arrayFilters: [{ 'element.date': id }] };
+        const update = {
+          $set: {
+            'appointments.$[element].appointment': appointment,
+            'appointments.$[element].control': control,
+            'appointments.$[element].date': date,
+            'appointments.$[element].price': price,
+            'appointments.$[element].technician': technician,
+            'appointments.$[element].treatment': treatment
+          }
+        };
+
+        clientsCollection
+          .updateOne(query, update, options)
+          .then((results) => success(res, results))
+          .catch((err) => error(err));
+      });
+
+      app.delete('/appointment/:date', (req, res) => {
+        const { date } = req.params;
+        const query = { 'appointments.date': date };
+        const update = { $pull: { appointments: { date } } };
+
+        clientsCollection
+          .updateOne(query, update)
+          .then((results) => success(res, results))
+          .catch((err) => error(err));
+      });
+    } else {
+      logErrorConnecting(err, client);
+    }
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
